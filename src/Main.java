f96293cb75be9e0d4440be95dad0f3207936ebce
Aryeh Zucker <159961161+AryehZucker@users.noreply.github.com>
2025-04-25 14:37:07 -0400

    Add driver code for getting a name and inserting it into the database

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,21 @@
+import java.io.IOException;
+import java.sql.SQLException;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+    public static void main(String[] args) throws IOException, SQLException {
+        String firstName, lastName;
+        try (UserInfo userInfo = new UserInfo()) {
+            firstName = userInfo.getFirstName();
+            lastName = userInfo.getLastName();
+        }
+
+        try (DatabaseConnection database = new DatabaseConnection()) {
+            boolean success = database.insertNameIntoDB(firstName, lastName);
+            if (!success) {
+                System.out.println("Name already exists in table");
+            }
+        }
     }
+
 }
